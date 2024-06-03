f9ec4a612854b904308fe0c26f12cdcad67f6905
Davendra Jayasingam <dave@theasianpokertour.com>
2024-06-03 23:26:43 +0800

    MVP

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  // detect service worker updates and reload the page
+  async function detectSWUpdate() {
+    const registration = await navigator.serviceWorker.getRegistration();
+    if (registration) {
+      registration.addEventListener("updatefound", () => {
+        const newWorker = registration.installing;
+        newWorker?.addEventListener("statechange", () => {
+          if (newWorker.state === "installed") {
+            if (navigator.serviceWorker.controller) {
+              alert("New content is available. Click OK to refresh.");
+              newWorker.postMessage({ type: "SKIP_WAITING" });
+              window.location.reload();
+            }
+          }
+        });
+      });
+    }
+  }
+
+  onMount(detectSWUpdate);
+</script>
+
+<slot  />
+
+<svelte:head>
+  <title>PWA Test Using SW</title>
+</svelte:head>
